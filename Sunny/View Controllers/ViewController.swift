555818c66d4541a17897e8ba49303b50e920a294
Maxim Mitin <maximus405@gmail.com>
2021-12-21 21:20:31 +0300

    adding escaping closure (free VC from URLsession functions) , add logic for separate city names

--- Sunny/View Controllers/ViewController.swift
+++ Sunny/View Controllers/ViewController.swift
@@ -19,7 +19,9 @@ class ViewController: UIViewController {
     
     
     @IBAction func searchPressed(_ sender: UIButton) {
-        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert) { city in
+            self.networkManager.fetchCurrentWeather(forCity: city)
+        }
     }
     
     override func viewDidLoad() {
